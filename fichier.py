002fb0ccf5d1b6009acc65f7637189b2cf05feac
abc <abc@example.com>
2021-09-08 20:07:02 +0200

    ver 1

---
+++ fichier.py
@@ -0,0 +1,14 @@
+valeur = int(input("Entrez une valeur pour X :"))
+
+def fonctionp(x):
+    p=x**2
+    return p
+    print(p)
+    
+def fonctionh(x):
+    h=x**3
+    return h
+    print(h)
+    
+print(fonctionp(valeur))
+print(fonctionh(valeur))
